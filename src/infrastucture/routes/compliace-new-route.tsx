c71b24d4ee77925f55b6c9dac7c722ac5be29a2b
Felipe Soares <felipeluizsoares@gmail.com>
2024-08-30 16:11:05 -0700

    ref: move route to files

---
+++ src/infrastucture/routes/compliace-new-route.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { useNavigate } from "react-router";
+import { ComplianceNewPage } from "../../pages/compliance/compliance-new.tsx";
+
+export const ComplianceNewRoute = () => {
+  const navigate = useNavigate();
+
+  return <ComplianceNewPage onSubmit={() => navigate("/compliance")} />;
+};
--- src/infrastucture/routes/route.tsx
+++ src/infrastucture/routes/route.tsx
@@ -3,35 +3,13 @@ import {
   createBrowserRouter,
   LoaderFunctionArgs,
   RouterProvider,
-  useNavigate,
-  useParams,
 } from "react-router-dom";
 import { complianceUseCase } from "../../compliance/use-case/compliance.usecase.ts";
-import { ComplianceEditPage } from "../../pages/compliance/compliance-edit.tsx";
-import { ComplianceNewPage } from "../../pages/compliance/compliance-new.tsx";
 import { CompliancePage } from "../../pages/compliance/compliance.tsx";
 import { LoginPage } from "../../pages/login-v2/login.tsx";
+import { ComplianceEditRoute } from "./compliace-edit-route.tsx";
+import { ComplianceNewRoute } from "./compliace-new-route.tsx";
 import { Root } from "./root-route.tsx";
-
-const ComplianceEditRoute = () => {
-  const { id } = useParams();
-
-  const navigate = useNavigate();
-
-  if (!id) {
-    return <div> Error loading the destination</div>;
-  }
-
-  return (
-    <ComplianceEditPage id={id} onSubmit={() => navigate("/compliance")} />
-  );
-};
-
-const ComplianceNewRoute = () => {
-  const navigate = useNavigate();
-
-  return <ComplianceNewPage onSubmit={() => navigate("/compliance")} />;
-};
 
 const router = createBrowserRouter([
   {
