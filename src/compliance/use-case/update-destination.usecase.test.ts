6e504808adae964b3a8691b1205a59d0bc190ef9
Felipe Soares <felipeluizsoares@gmail.com>
2024-08-30 22:27:07 -0700

    ref: add test to use case

---
+++ src/compliance/use-case/update-destination.usecase.test.ts
@@ -0,0 +1,51 @@
+import { proxy } from "valtio";
+import {
+  ComplianceStore,
+  newDestinationInitialState,
+} from "../store/compliance.store";
+import { updateDestination } from "./update-destination.usecase";
+import { destinationMock } from "../mock/compliance.mock";
+
+describe("updateDestination", () => {
+  let complianceStore: ComplianceStore;
+
+  beforeEach(() => {
+    complianceStore = proxy<ComplianceStore>({
+      destinations: {
+        value: [destinationMock()],
+        error: null,
+        isLoading: false,
+      },
+      newDestination: newDestinationInitialState(),
+    });
+  });
+
+  it("should update the destination on the destinations list", async () => {
+    const id = destinationMock().destination;
+    const updatedLocation = "test updated location";
+
+    complianceStore.newDestination = {
+      ...destinationMock(),
+      location: updatedLocation,
+    };
+
+    await updateDestination({ id, complianceStore });
+
+    expect(complianceStore.destinations.value[0].location).toEqual(
+      updatedLocation
+    );
+  });
+
+  it("should reset newDestination to initial state", async () => {
+    complianceStore.newDestination = destinationMock();
+
+    await updateDestination({
+      id: destinationMock().destination,
+      complianceStore,
+    });
+
+    expect(complianceStore.newDestination).toEqual(
+      newDestinationInitialState()
+    );
+  });
+});
